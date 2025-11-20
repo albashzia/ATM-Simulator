2520edc45a9865d634475fc62562c93925362379
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2025-11-20 18:41:56 +0000

    Bringing the input intake under while and defining basic if-else structure

--- ATM_Simulator.py
+++ ATM_Simulator.py
@@ -2,4 +2,10 @@ print("Welcome to ATM-Simulator")
 pin = 1234
 balance = 1000
 attempts = 0
-entered_pin = int(input("Enter your pin: "))
+while(attempts < 3):
+    entered_pin = int(input("Enter your pin: "))
+    if(entered_pin == pin):
+        print("Choose the operation to perform")
+    else:
+        print("Wrong pin, enter your pin again")
+        attempts = attempts + 1
