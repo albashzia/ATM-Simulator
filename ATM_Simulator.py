23596e2aa94b28bc598b3456515a257c5bd5b5dd
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2025-11-20 18:44:13 +0000

    Adding break to break the loop on correct pin entered

--- ATM_Simulator.py
+++ ATM_Simulator.py
@@ -6,6 +6,7 @@ while(attempts < 3):
     entered_pin = int(input("Enter your pin: "))
     if(entered_pin == pin):
         print("Choose the operation to perform")
+        break;
     else:
         print("Wrong pin, enter your pin again")
         attempts = attempts + 1
